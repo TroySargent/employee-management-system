a1b98a43d3704281279fdfc51d35cee3866484c6
Troy Sargent <troyjsargent@gmail.com>
2020-08-24 14:39:00 -0500

    queries and prompt for adding work

--- addObject.js
+++ addObject.js
@@ -1,11 +1,38 @@
 const inquirer = require("inquirer");
 const connection = require("./connection");
+const choices = require("./choiceList");
 
-const addObject = (object) => {
-    connection.query("SELECT * FROM employees", (err, data) => {
-        if (err) {throw err};
-        console.table(data);
-    });
+const addObject = async () => {
+    await getEmployeeToAdd(await choices.getRoleList(), await choices.getEmployeeList());
 };
 
+const getEmployeeToAdd = async (roleList, employeeNames) => {
+    let info = await inquirer.prompt([{
+        name: "firstName",
+        message: "What is the employee's first name?",
+        type: "input"
+    },
+    {
+        name: "lastName",
+        message: "What is the employee's last name?",
+        type: "input"
+    },
+    {
+        name: "role",
+        message: "What is the employee's role?",
+        type: "list",
+        choices: roleList
+    },
+    {
+        name: "mananger",
+        message: "What is the employee's manager?",
+        type: "list",
+        choices: employeeNames
+    }]);
+    await addEmployeeToTable(info);
+};
+
+const addEmployeeToTable = (info) => {
+    console.log(info)
+}
 module.exports = addObject;
