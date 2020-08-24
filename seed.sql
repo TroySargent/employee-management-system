d4b30af672e8511bf0e3569d0b9a2c4d4c0c3f1f
Troy Sargent <troyjsargent@gmail.com>
2020-08-24 15:04:32 -0500

    added auto_increment to id fields in schema

--- seed.sql
+++ seed.sql
@@ -5,30 +5,30 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-id INT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-id INT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
-salaray DECIMAL,
+salary DECIMAL,
 departmentID INT NOT NULL
 );
 
 CREATE TABLE employees (
-id INT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 firstName VARCHAR(30) NOT NULL,
 lastName VARCHAR(30) NOT NULL,
 roleID INT NOT NULL,
 managerID INT DEFAULT NULL
 );
 
-INSERT INTO department 
-VALUES (1, "Financial Services") , (2, "Sales"), (3, "Customer Solutions"), (4, "Engineering");
+INSERT INTO department (name)
+VALUES ("Financial Services") , ("Sales"), ("Customer Solutions"), ("Engineering");
 
-INSERT INTO role
-VALUES (1, "CEO", 150000, 1), (2, "Sales", 90000, 2), (3, "Customer Representative", 60000, 3), (4, "Software Engineer", 100000, 4);
+INSERT INTO role (title, salary, departmentID)
+VALUES ("CEO", 150000, 1), ("Sales", 90000, 2), ("Customer Representative", 60000, 3), ("Software Engineer", 100000, 4);
 
-INSERT INTO employees 
-VALUES (1, "Frederick", "Douglass", 1, NULL), (2, "Elizabeth", "Stanton", 2, 1), (3, "George", "Washington", 3, 1), (4, "Bob", "Ross", 4, 1);
+INSERT INTO employees (firstName, lastName, roleID, managerID)
+VALUES ("Frederick", "Douglass", 1, NULL), ("Elizabeth", "Stanton", 2, 1), ("George", "Washington", 3, 1), ("Bob", "Ross", 4, 1);
